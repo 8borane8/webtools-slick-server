a6591d3baad4f154cbb00557647bdfc24d8017f6
8borane8 <74107604+8borane8@users.noreply.github.com>
2026-06-13 01:15:11 +0100

    fix

--- src/islands/hydration.ts
+++ src/islands/hydration.ts
@@ -1,30 +1,15 @@
 import * as esbuild from "esbuild";
+import * as path from "@std/path";
 
-export const islandHydration = () => {
-	const hydrated = new WeakSet<Element>();
+const CLIENT_ENTRY = path.fromFileUrl(import.meta.resolve("./client.ts"));
 
-	async function run(container: Document | Element) {
-		const roots = Array.from(container.querySelectorAll("[data-slick-island]")).filter((r) => !hydrated.has(r));
-		if (!roots.length) return;
+const { outputFiles } = await esbuild.build({
+	entryPoints: [CLIENT_ENTRY],
+	bundle: true,
+	format: "esm",
+	minify: true,
+	write: false,
+	external: ["preact", "preact-root-fragment"],
+});
 
-		const { createRootFragment } = await import("preact-root-fragment");
-		const { hydrate, h } = await import("preact");
-
-		await Promise.all(roots.map(async (root: Element) => {
-			hydrated.add(root);
-			const name = root.getAttribute("data-slick-island");
-			const props = JSON.parse(root.getAttribute("data-slick-props") || "{}");
-
-			const { default: Island } = await import("/~islands/" + name);
-			hydrate(h(Island, props), createRootFragment(root.parentElement!, root));
-		}));
-	}
-
-	run(document);
-	new MutationObserver(() => run(document)).observe(document, { childList: true, subtree: true });
-};
-
-export const { code: ISLAND_HYDRATION_SCRIPT } = await esbuild.transform(
-	`(${islandHydration.toString()})()`,
-	{ minify: true, format: "esm" },
-);
+export const ISLAND_HYDRATION_SCRIPT = new TextDecoder().decode(outputFiles[0].contents);
